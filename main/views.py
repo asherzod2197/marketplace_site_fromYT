8e7a0a3dbeedd9d2760902ce8c444fbd7a0e25be
asherzod2197 <asherzod2197@gmail.com>
2026-03-28 19:43:51 +0500

    fixed

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from prducts.models import Product
 
 # Create your views here.
+
+class IndexView(View):
+    def get (request):
+        products = Product.objects.all()
+
+        context = {
+            "products": products
+        }
+        return render(request, "index.html", context=context)
+
+
